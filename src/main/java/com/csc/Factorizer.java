98101695c268b1186de5136bdae1283e7cd03961
Ahtisham8 <84299800+Ahtisham8@users.noreply.github.com>
2024-09-09 13:38:08 -0700

    Finished the Factorizer class and tests
    Factorizer class only returns the prime factors of any given number,
    
    The test class tests for various numbers to calculate and test different situations.

--- src/main/java/com/csc/Factorizer.java
+++ src/main/java/com/csc/Factorizer.java
@@ -2,13 +2,43 @@ package com.csc;
 
 public class Factorizer {
 
+
+
   // You should implement your solution here.
   // Feel free to delete this example method when you implement your solution.
-  public int exampleMethod(int someArgument) {
-    if (someArgument > 5) {
-      return 1;
-    } else {
-      return 2;
+  public String exampleMethod(int someArgument) {
+    
+    String output = "";
+    String finalOutput = "";
+
+
+    if (someArgument == 0 || someArgument == 1) {
+      return null;
     }
+
+    for(int i = 2; i < someArgument; i++)
+    {
+
+        
+        while(someArgument % i == 0)
+        {
+        
+            output += i + ", ";
+            someArgument = someArgument/i;
+        }
+    }
+
+    if (someArgument > 2) {
+      output += someArgument;
+    }
+
+  
+    
+    finalOutput = output.trim();
+    finalOutput = finalOutput.replaceAll(",$", "");
+    
+    return finalOutput;
+    
   }
 }
+
--- src/test/java/com/csc/TestFactorizer.java
+++ src/test/java/com/csc/TestFactorizer.java
@@ -18,16 +18,52 @@ public class TestFactorizer {
   // of the prime factorization function.
   @Test
   public void testZero() {
-    assertEquals(2, factorizer.exampleMethod(0));
+    assertEquals(null, factorizer.exampleMethod(0));
   }
 
   @Test
   public void testOne() {
-    assertEquals(2, factorizer.exampleMethod(1));
+    assertEquals(null, factorizer.exampleMethod(1));
   }
 
   @Test
   public void testTen() {
-    assertEquals(1, factorizer.exampleMethod(10));
+    assertEquals(2 + ", " + 5, factorizer.exampleMethod(10));
   }
+
+  @Test
+  public void testNine() {
+    assertEquals(3 + ", " + 3, factorizer.exampleMethod(9));
+  }
+
+  @Test
+  public void testFourtyTwo() {
+    assertEquals(2 + ", " + 3 + ", " + 7, factorizer.exampleMethod(42));
+  }
+
+  @Test
+  public void testHundred() {
+    assertEquals(2 + ", " + 2 + ", " + 5 + ", " + 5, factorizer.exampleMethod(100));
+  }
+
+  @Test
+  public void testThirtyFive() {
+    assertEquals(5 + ", " + 7, factorizer.exampleMethod(35));
+  }
+
+  @Test
+  public void testTwentyFour() {
+    assertEquals(2 + ", " + 2 + ", " + 2 + ", " + 3, factorizer.exampleMethod(24));
+  }
+
+  @Test
+  public void testSeventeen() {
+    assertEquals(17 + "", factorizer.exampleMethod(17));
+  }
+
+  @Test
+  public void testThirteen() {
+    assertEquals(13 + "", factorizer.exampleMethod(13));
+  }
+
 }
